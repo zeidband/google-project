72747566302a173206ee33852e57436682638f09
Sari Seidband <zs@sem-wolf.org>
2020-07-22 13:29:54 +0300

    add the delete character function ande checked it and add the replace character without check

--- complete.py
+++ complete.py
@@ -1,26 +1,35 @@
-import read_data as r
-import glob
+import string
 
-def get_best_k_completions(string):
-    # sentences = r.init_data(glob.glob("technology_texts/RFC/*.txt"))
+def delete_character(input_, sentences):
+    for i in range(len(input_)):
+        if input_[:i] + input_[i + 1:] in sentences.keys():
+            print("FOR DEBUG: delete sentence")
+            return sentences[input_[:i] + input_[i + 1:]], i
 
-    sentences = r.init_data(["technology_texts/python-3.8.4-docs-text/python-3.8.4-docs-text/about.txt"])
-    sentence = input()
-
-    while sentence != '#':
-        flag = False
-
-        try:
-            set_id_sentences = sentences[sentence]
-
-        except ValueError:
-            flag = True
-
-        if flag:
-            return None
-
-        else:
-            max_5_list()
+    return None, -1
 
 
-        sentence = input()
+def replace_character(sentence, sentences):
+    result = set()
+    for letter in string.ascii_letters:
+        for character in range(len(sentence)):
+            change_sentence = sentence[:character] + letter + sentence[character + 1:]
+            if change_sentence in sentences.keys():
+                result.union(sentences[change_sentence])
+                if len(result) >= 5:
+                    return result[:5]
+
+def get_best_k_completions(sentence, data, sentences):
+    # check if the sentence is complete in the data
+    if sentence in sentences.keys():
+        return sentences[sentence]
+
+    # else try to delete character
+    set_id_sentences, key = delete_character(sentence, sentences)
+    if set_id_sentences is not None:
+        return set_id_sentences
+    # TODO: check if the answer len = 5
+
+    replace_character(sentence, sentences)
+
+    return None
