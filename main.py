aa3555865a72fae4b62719246f608d2c833717be
Sari Seidband <zs@sem-wolf.org>
2020-07-31 10:45:02 +0300

    input correction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from complete import Complete
 from read_data import Data
 import os
+import string
 
 def get_file_list(path):
     file_list = []
@@ -11,26 +12,35 @@ def get_file_list(path):
     return file_list
 
 def start():
-    print("Loading the file and preparing the system...")
+    print("Loading the files and preparing the system...")
 
-    files_list = get_file_list("technology_texts/python-3.8.4-docs-text/python-3.8.4-docs-text")
-    # ["technology_texts/python-3.8.4-docs-text/python-3.8.4-docs-text/about.txt"]
+    files_list = get_file_list("technology_texts/python-3.8.4-docs-text/python-3.8.4-docs-text/c-api")
+    # files_list = get_file_list("technology_texts/python-3.8.4-docs-text/python-3.8.4-docs-text/whatsnew")
     complete = Complete(Data(files_list))
 
-    print("The system is ready.\n\nEnter your text: ")
+    # complete = Complete(Data(["technology_texts/python-3.8.4-docs-text/python-3.8.4-docs-text/about.txt"]))
 
-    sentence = input()
+    print("The system is ready.")
 
-    while sentence != '#':
-        match_sentences = complete.get_best_k_completions(sentence)
+    input_ = ' '.join((''.join(i for i in input("\n\nEnter your text: ") if i in string.ascii_letters + ' ')).split())
 
-        if len(match_sentences) != 0:
-            for sentence in match_sentences:
-                print(f"{sentence.completed_sentence} ({files_list[sentence.source_text]} {sentence.offset})")
-        else:
-            print("there is no items")
+    while(1):
+        text = " "
 
-        sentence = input("\n\nEnter your text: ")
+        while text[-1] != '#':
+            match_sentences = complete.get_best_k_completions(input_)
+
+            if len(match_sentences) != 0:
+                for sentence in match_sentences:
+                    print(f"{sentence.completed_sentence} ({files_list[sentence.source_text]} {sentence.offset})")
+            else:
+                print("there is no items")
+
+            # print(input_, end="")
+            text = input(input_)
+            input_ += ' '.join((''.join(i for i in text if i in string.ascii_letters + ' ')).split())
+
+        input_ = ' '.join((''.join(i for i in input("\n\nEnter your text: ") if i in string.ascii_letters + ' ')).split())
 
 
 if __name__ == '__main__':
